20d3e0fcb1991e69b79060feb954b768866e8640
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2023-05-24 19:14:57 -0700

    kind of fixed button issue - still needs work

--- src/Game.java
+++ src/Game.java
@@ -10,15 +10,12 @@
 import javax.swing.*;
 import java.awt.*;
 import java.net.URL;
-import java.util.Objects;
 
 public class Game {
 
     private JFrame frame;
     private JPanel panel;
     private JButton btn_move, btn_take_role, btn_rehearse, btn_act;
-    private JLabel playerLabel;
-    private JTextArea playerStatsArea;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new Game().GUI());
@@ -29,30 +26,44 @@ public class Game {
         panel = new JPanel(); // Create a panel to hold all other components
         panel.setLayout(new BorderLayout()); // Use BorderLayout for panel
 
-        JPanel buttonPanel = new JPanel(new GridLayout(2, 2)); // Create a separate panel for the buttons
-        btn_move = new JButton("Move"); // Create buttons
-        btn_take_role = new JButton("Take Role");
-        btn_rehearse = new JButton("Rehearse");
-        btn_act = new JButton("Act");
+        JPanel buttonPanel = new JPanel(); // Create a separate panel for the buttons
+//        buttonPanel.setLayout(new GridLayout(2, 2, 10, 10)); // Use BoxLayout for the button panel
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
 
-        buttonPanel.add(btn_move); // Add the buttons to the button panel
-        buttonPanel.add(btn_take_role);
-        buttonPanel.add(btn_rehearse);
-        buttonPanel.add(btn_act);
+        Dimension buttonSize = new Dimension(100, 50); // Set the size of the buttons
+
+        JPanel topButtons = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        topButtons.add(createButton("Move", buttonSize, 100, 0)); // Add the buttons to the button panel
+        topButtons.add(createButton("Take Role", buttonSize, 100, 0));
+
+        JPanel bottomButtons = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        bottomButtons.add(createButton("Rehearse", buttonSize, 0, 300));
+        bottomButtons.add(createButton("Act", buttonSize, 0, 300));
+
+        buttonPanel.add(topButtons);
+        buttonPanel.add(bottomButtons);
 
         JPanel statsPanel = new JPanel(); // Create a panel for player stats
         statsPanel.setLayout(new BoxLayout(statsPanel, BoxLayout.Y_AXIS)); // Use BoxLayout for statsPanel
         statsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add a border to the panel
-        playerLabel = new JLabel("Current Player: "); // Create a label to display player stats
-        playerStatsArea = new JTextArea(5, 2); // Create a text area to display player stats
-        statsPanel.add(playerLabel); // Add the stats label to the stats panel
-        statsPanel.add(playerStatsArea); // Add the text area to the stats panel
+
+        JLabel playerName = createLabel("Player Name: ", 20, 10); // Create a label to display the player's name
+        JLabel playerRank = createLabel("Rank: ", 20, 10); // Create a label to display the player's rank
+        JLabel playerDollars = createLabel("Dollars: ", 20, 10); // Create a label to display the player's dollars
+        JLabel playerCredits = createLabel("Credits: ", 20, 10); // Create a label to display the player's credits
+        JLabel playerChips = createLabel("Practice Chips: ", 20, 10); // Create a label to display the player's chips
+
+        statsPanel.add(playerName); // Add the stats label to the stats panel
+        statsPanel.add(playerRank);
+        statsPanel.add(playerDollars);
+        statsPanel.add(playerCredits);
+        statsPanel.add(playerChips);
 
 
         // Setup right panel to contain player stats and buttons
         JPanel rightPanel = new JPanel(new BorderLayout());
         rightPanel.add(statsPanel, BorderLayout.NORTH);
-        rightPanel.add(buttonPanel, BorderLayout.SOUTH);
+        rightPanel.add(buttonPanel, BorderLayout.CENTER);
 
 
         JLayeredPane layeredPane = new JLayeredPane(); // Create the JLayeredPane to hold the board, cards, tokens, etc.
@@ -61,7 +72,7 @@ public class Game {
         URL boardURL = getClass().getResource("/images/board.jpg");
         ImageIcon board = new ImageIcon(boardURL); // Create the board image
         layeredPane.setPreferredSize(new Dimension(board.getIconWidth(), board.getIconHeight())); // Set the size of the game board
-        board.setImage(board.getImage().getScaledInstance(board.getIconWidth(), board.getIconHeight(), Image.SCALE_DEFAULT)); // Scale the board image to fit the board
+        board.setImage(board.getImage().getScaledInstance(board.getIconWidth(), board.getIconHeight(), Image.SCALE_AREA_AVERAGING)); // Scale the board image to fit the board
         JLabel boardLabel = new JLabel(board); // Add the board image to a label
         boardLabel.setBounds(0, 0, board.getIconWidth(), board.getIconHeight()); // Set the size of the board label
 
@@ -105,6 +116,22 @@ public class Game {
             System.out.println("Game Setup Cancelled!");
             // exit
         }
+    }
+
+    private JLabel createLabel(String text, int fontSize, int spacing) {
+        JLabel label = new JLabel(text);
+        label.setFont(new Font("Serif", Font.BOLD, fontSize));
+        label.setBorder(BorderFactory.createEmptyBorder(0, 0, spacing, 0));
+        return label;
+    }
+
+    private JPanel createButton(String buttonName, Dimension buttonSize, int top, int bottom) {
+        JButton button = new JButton(buttonName);
+        button.setPreferredSize(buttonSize);
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(button);
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(top, 0, bottom, 0));
+        return buttonPanel;
     }
 }
 
