15a9a4e2bcf6c7b479916f8ee662a62f66e29c82
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2023-05-27 23:43:00 -0700

    refactored, made player info display match player colors, and implemented end turn functionality

--- src/Game.java
+++ src/Game.java
@@ -9,56 +9,131 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.Map;
+
+import static javax.swing.text.StyleConstants.setForeground;
 
 public class Game {
     private GameManager manager;
     private JFrame frame;
-    private JPanel panel;
+    private JPanel panel, buttonPanel, statsPanel;
     private JLayeredPane layeredPane;
-    private JLabel playerName;
-    private JLabel playerRank;
-    private JLabel playerDollars;
-    private JLabel playerCredits;
-    private JLabel playerChips;
+    private JLabel playerName, playerRank, playerDollars, playerCredits, playerChips;
+
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new Game().GUI());
     }
+
 
     private void GUI() {
         frame = new JFrame("Deadwood"); // Create and set up the window.
         panel = new JPanel(); // Create a panel to hold all other components
         panel.setLayout(new BorderLayout()); // Use BorderLayout for panel
 
-        JPanel buttonPanel = new JPanel(); // Create a separate panel for the buttons
-//        buttonPanel.setLayout(new GridLayout(2, 2, 10, 10)); // Use BoxLayout for the button panel
+        // initialize the buttons
+        setupButtons();
+
+        // initialize the stats panel
+        setupStats();
+
+        // Setup right panel to contain player stats and buttons
+        setupRightPanel();
+
+        // Setup the board
+        setupBoard();
+
+        frame.add(panel); // Add the panel to the frame
+        frame.setSize(1500, 1000); // Set the size of the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit program when the frame is closed
+        frame.setVisible(true); // Make the frame visible
+
+        // set number of players and start game
+        selectPlayers();
+
+        // option to rename players
+        renamePlayers();
+
+        // display current player stats
+        currentPlayerInfo();
+
+        // display cards on board
+        showCards();
+
+        // display shot counters on board
+        showTakes();
+
+        // display player tokens on board
+        showTokens();
+    }
+
+
+    private void setupButtons() {
+        buttonPanel = new JPanel(); // Create a separate panel for the buttons
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
 
         Dimension buttonSize = new Dimension(100, 50); // Set the size of the buttons
 
-        JPanel topButtons = new JPanel(new FlowLayout(FlowLayout.CENTER)); // Create a panel for the buttons
-        topButtons.add(createButton("Move", buttonSize, 200, 0)); // Add the buttons to the button panel
-        topButtons.add(createButton("Take Role", buttonSize, 200, 0));
-
+        JPanel topButtons = new JPanel(new FlowLayout(FlowLayout.CENTER)); // Create panels for button order
         JPanel middleButtons = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        middleButtons.add(createButton("Rehearse", buttonSize, 10, 10));
-        middleButtons.add(createButton("Act", buttonSize, 10, 10));
-
         JPanel bottomButtons = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        bottomButtons.add(createButton("Upgrade", buttonSize, 0, 200)); // Add the buttons to the button panel
-        bottomButtons.add(createButton("End Turn", buttonSize, 0, 200));
+
+        ActionListener moveListener = e -> {
+            manager.move("location"); // TODO - move logic
+            currentPlayerInfo();
+        };
+
+        ActionListener takeRoleListener = e -> {
+            manager.takeRole("role"); // TODO - take role logic
+            currentPlayerInfo();
+        };
+
+        ActionListener rehearseListener = e -> {
+            manager.rehearse(); // TODO - rehearse logic
+            currentPlayerInfo();
+        };
+
+        ActionListener actListener = e -> {
+            manager.act(); // TODO - act logic
+            currentPlayerInfo();
+        };
+
+        ActionListener upgradeListener = e -> {
+            Area area = new Area(0, 0, 0, 0); // dummy area
+            Upgrade upgrade = new Upgrade(0, "c", 0, area); // dummy upgrade
+            manager.upgrade(upgrade, "$"); // TODO - upgrade logic
+            currentPlayerInfo();
+        };
+
+        ActionListener endTurnListener = e -> {
+            manager.endTurn();
+            currentPlayerInfo();
+        };
+
+
+        topButtons.add(createButton("Move", buttonSize, 200, 0, moveListener)); // Add the buttons to the button panel
+        topButtons.add(createButton("Take Role", buttonSize, 200, 0, takeRoleListener));
+
+        middleButtons.add(createButton("Rehearse", buttonSize, 10, 10, rehearseListener));
+        middleButtons.add(createButton("Act", buttonSize, 10, 10, actListener));
+
+        bottomButtons.add(createButton("Upgrade", buttonSize, 0, 200, upgradeListener)); // Add the buttons to the button panel
+        bottomButtons.add(createButton("End Turn", buttonSize, 0, 200, endTurnListener));
 
         buttonPanel.add(topButtons); // Add the button panel to the main panel
         buttonPanel.add(middleButtons);
         buttonPanel.add(bottomButtons);
+    }
 
-        JPanel statsPanel = new JPanel(); // Create a panel for player stats
+
+    private void setupStats() {
+        statsPanel = new JPanel(); // Create a panel for player stats
         statsPanel.setLayout(new BoxLayout(statsPanel, BoxLayout.Y_AXIS)); // Use BoxLayout for statsPanel
         statsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add a border to the panel
 
-        playerName = createLabel("", 20, 15); // Create a label to display the player's name
+        playerName = createLabel("", 25, 15); // Create a label to display the player's name
         playerRank = createLabel("", 15, 10); // Create a label to display the player's rank
         playerDollars = createLabel("", 15, 10); // Create a label to display the player's dollars
         playerCredits = createLabel("", 15, 10); // Create a label to display the player's credits
@@ -69,55 +144,33 @@ public class Game {
         statsPanel.add(playerDollars);
         statsPanel.add(playerCredits);
         statsPanel.add(playerChips);
+    }
 
 
-        // Setup right panel to contain player stats and buttons
+    private void setupRightPanel() {
         JPanel rightPanel = new JPanel(new BorderLayout());
         rightPanel.add(statsPanel, BorderLayout.NORTH);
         rightPanel.add(buttonPanel, BorderLayout.CENTER);
+        panel.add(rightPanel, BorderLayout.EAST); // Add the right panel to the panel
+    }
 
 
+    private void setupBoard() {
         layeredPane = new JLayeredPane(); // Create the JLayeredPane to hold the board, cards, tokens, etc.
-
 
         ImageIcon board = getImage("/resources/images/board.jpg"); // Create the board image
         layeredPane.setPreferredSize(new Dimension(board.getIconWidth(), board.getIconHeight())); // Set the size of the game board
+
         JLabel boardLabel = new JLabel(board); // Add the board image to a label
         boardLabel.setBounds(0, 0, board.getIconWidth(), board.getIconHeight()); // Set the size of the board label
 
-        // testing - dummy player icon
-//        ImageIcon playerIcon = getImage("/resources/images/tokens/r1.png");
-//        JLabel player = new JLabel(playerIcon);
-//        player.setBounds(0, 0, playerIcon.getIconWidth(), playerIcon.getIconHeight());
-
-        // testing - layering
-        layeredPane.add(boardLabel, Integer.valueOf(0));
-//        layeredPane.add(player, Integer.valueOf(2));
-
+        layeredPane.add(boardLabel, Integer.valueOf(0)); // Add the board to the lowest layer
 
         panel.add(layeredPane, BorderLayout.CENTER); // Add the layered pane to the panel
-        panel.add(rightPanel, BorderLayout.EAST); // Add the right panel to the panel
-
-        frame.add(panel); // Add the panel to the frame
-        frame.setSize(1500, 1000); // Set the size of the frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        selectPlayers();
-
-        renamePlayers(manager);
-
-        currentPlayerInfo();
-        // TODO - refactor to allow single card images to be replaced and redisplayed
-        showCards();
-
-        showTakes();
-
-        showTokens();
-
-
     }
 
+
+    // TODO - refactor to allow single card images to be replaced and redisplayed
     private void showCards() {
         manager.getCards().forEach((card, area) -> {
             String path = card.getImg();
@@ -131,6 +184,7 @@ public class Game {
             layeredPane.add(cardLabel, Integer.valueOf(1));
         });
     }
+
 
     private void showTakes() {
         manager.getTakes().forEach((take, area) -> {
@@ -146,6 +200,7 @@ public class Game {
         });
     }
 
+
     private void showTokens() {
         manager.getTokens().forEach((path, position) -> {
             int x = position[0];
@@ -157,9 +212,24 @@ public class Game {
         });
     }
 
+
     private void currentPlayerInfo() {
         Player currentPlayer = manager.getCurrentPlayer();
-        playerName.setText("Current Player: " + currentPlayer.getName());
+
+        String color = currentPlayer.getColor();
+        switch (color) {
+            case "b" -> playerName.setForeground(Color.BLUE);
+            case "c" -> playerName.setForeground(Color.CYAN);
+            case "g" -> playerName.setForeground(Color.GREEN);
+            case "o" -> playerName.setForeground(Color.ORANGE);
+            case "p" -> playerName.setForeground(Color.PINK);
+            case "r" -> playerName.setForeground(Color.RED);
+            case "v" -> playerName.setForeground(Color.MAGENTA);
+            case "w" -> playerName.setForeground(Color.WHITE);
+            case "y" -> playerName.setForeground(Color.YELLOW);
+        }
+
+        playerName.setText(currentPlayer.getName());
         playerRank.setText("Rank: " + currentPlayer.getRank());
         playerDollars.setText("Dollars: " + currentPlayer.getDollars());
         playerCredits.setText("Credits: " + currentPlayer.getCredits());
@@ -174,14 +244,17 @@ public class Game {
         return label;
     }
 
-    private JPanel createButton(String buttonName, Dimension buttonSize, int top, int bottom) {
+
+    private JPanel createButton(String buttonName, Dimension buttonSize, int top, int bottom, ActionListener action) {
         JButton button = new JButton(buttonName);
         button.setPreferredSize(buttonSize);
+        button.addActionListener(action);
         JPanel buttonPanel = new JPanel(new FlowLayout());
         buttonPanel.add(button);
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(top, 0, bottom, 0));
         return buttonPanel;
     }
+
 
     private void selectPlayers() {
         // Player selection dialog
@@ -206,7 +279,8 @@ public class Game {
         }
     }
 
-    private void renamePlayers(GameManager manager) {
+
+    private void renamePlayers() {
         int renameOption = JOptionPane.showConfirmDialog(null, "Would you like to enter custom player names?", "Rename Players", JOptionPane.YES_NO_OPTION);
         if(renameOption == JOptionPane.YES_OPTION) {
             for(Player player : manager.getPlayers()) {
@@ -215,6 +289,7 @@ public class Game {
             }
         }
     }
+
 
     private String renamePrompt(String name) {
         JTextField input = new JTextField();
@@ -226,6 +301,7 @@ public class Game {
         return input.getText();
     }
 
+
     private ImageIcon getImage(String path) {
         URL url = getClass().getResource(path);
         ImageIcon image = new ImageIcon(url);
@@ -233,5 +309,3 @@ public class Game {
         return image;
     }
 }
-
-
